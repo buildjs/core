4b42562a24e30f8e44c72afe936d1e9c2a7ed94c
Damon Oehlman <damon.oehlman@sidelab.com>
2012-11-02 08:52:11 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# BuildJS Core Tools
+
+This library is a set of core settings and utilities that are shared across the BuildJS tools.  The library is made up of a number of modules that can be accessed by using `require('buildjs.core/modulename')`.  The list of modules and their purpose is outlined below:
+
+* `buildjs.core/regexes` - Regular expressions that are used within the BuildJS tools.  Prior to the creation of this core library there was a lot of repitition (and opportunity for error) with regular expressions in each of the modules.
---
+++ regexes.js
@@ -0,0 +1,40 @@
+var regexes = module.exports = {
+    lineBreak: /\n/,
+    trailingReturn: /\r$/,
+    leadingDot = /^\./,
+    trailingDot = /\.$/,
+    leadingSlash = /^\//,
+    trailingSlash = /\/$/,
+    multiTarget = /^(.*?)\[(.*)\]$/,
+
+    // alias definitions: blah!optional-trailing-section
+    alias = /^([\w\-]+)\!(.*)$/,
+
+    // js single line include //=
+    includeDoubleSlash = /^(\s*)\/\/\=(\w*)\s*(.*)$/,
+
+    // css, js multiline include /*=  */
+    includeSlashStar = /^(\s*)\/\*\=(\w*)\s*(.*?)\s*\*\/$/,
+
+    // coffeescript single line include #=
+    includeHash = /^(\s*)\#\=(\w*)\s*(.*)$/,
+
+    // leading and trailing quote capture
+    quotesLeadAndTrail = /(^[\"\']|[\"\']$)/g,
+
+    fallbackDelim = /\s+\:\s+/
+};
+
+// specify the include regexes
+// these regexes specify the include patterns that are supported for different
+// types of files
+regexes.includes = {
+    // core supported file types
+    js:     [ regexes.includeDoubleSlash, regexes.includeSlashStar ],
+    css:    [ regexes.includeSlashStar ],
+
+    // other cool languages that I use every now and again
+    coffee: [ regexes.includeHash ],
+    roy:    [ regexes.includeDoubleSlash ],
+    styl:   [ regexes.includeDoubleSlash ]
+};
